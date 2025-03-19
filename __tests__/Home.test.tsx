7baf94028d5cb059bafbfd2b809988420ebec8f1
Alessandra Varisco <alessandra.varisco@kukenantech.com>
2025-03-19 13:26:21 +0100

    Cleaning tests files

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -7,10 +7,6 @@ import { NavigationContainer } from '@react-navigation/native';
 
 describe('Home View', () => {
 
-    const defaultProps = {
-        onPress: jest.fn(),
-      };
-
     const HomeView = render(
         <Provider store={store}>
             <NavigationContainer>
@@ -25,14 +21,4 @@ describe('Home View', () => {
         expect(getByTestId('home-view')).toBeTruthy();
     });
 
-    // it('onpress renders correctly home', () => {
-    //     const { getByTestId } = HomeView;
-    //     expect(getByTestId('home-view')).toBeTruthy();
-    //     const onPress1 = getByTestId('home-view-click-1');
-    //     const fire = fireEvent.press(onPress1);
-    //     // expect(defaultProps.onPress).toHaveBeenCalled();
-    //     // const coord2 = getByTestId('content-value-1').children;
-    //     // expect(Number(coord1) && (Number(coord1) > 0 || Number(coord1) < 0)).toBe(true);
-    //     // expect(Number(coord2) && (Number(coord2) > 0 || Number(coord2) < 0)).toBe(true);
-    // });
 });
